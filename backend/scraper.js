89ceebcdcd1d2d853e56e2866ea864676044a454
KunalBhika <kunalbhika2@gmail.com>
2025-03-15 00:38:51 +0530

    data scraper.js completed

--- backend/scraper.js
+++ backend/scraper.js
@@ -2,25 +2,29 @@ import puppeteer from 'puppeteer';
 import axios from "axios";
 
 const scrapeJobs = async (keyword) => {
-    // https://www.naukri.com/product-manager-jobs?k=product%20manager
+    // https://www.naukri.com/product-manager-jobs?k=product%20manager - sample link
     const url = `https://www.naukri.com/${encodeURIComponent(keyword)}-jobs?k=${encodeURIComponent(keyword)}`;
 
-    const browser = await puppeteer.launch({ headless: false }); // headless mode
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     
     await page.goto(url);
 
-    // // Wait for job listings to appear
-    await page.waitForSelector(".comp-name", { timeout: 10000 });
+    // Wait for job post container to load for atleast 10 seconds
+    await page.waitForSelector(".styles_job-listing-container__OCfZC", { timeout: 10000 });
 
-    // Extract job titles
     const jobs = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".comp-name")).map(job => job.innerText);
+        return Array.from(document.querySelectorAll(".cust-job-tuple")).map(
+            job => ({
+                title : job.querySelector("a.title")?.innerText.trim() || "N/A" ,
+                company_name : job.querySelector(".comp-name")?.innerText.trim() || "N/A" ,
+                experience : job.querySelector(".expwdth")?.innerText.trim() || "N/A" ,
+                apply_link : job.querySelector(".comp-name")?.getAttribute("href") || "N/A" ,
+            })
+        )
     });
 
-    console.log("Jobs : " , jobs);
     await browser.close();
-    
 }
 
 export default scrapeJobs;
