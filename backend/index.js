4c9d3be79e82c4dd1422dab28ab5b54e276f487b
KunalBhika <kunalbhika2@gmail.com>
2025-03-15 16:24:30 +0530

    project completed

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,13 @@ connectToDB();
 // Api route
 app.use("/api/", findRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../frontend/dist")));
+    app.use("*", (req, res) => {
+        res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`App listening to PORT : ${PORT}`));
